6ccb6fcd38e20131aa4081c6040c0370181cc190
Martín García <tin.geka@gmail.com>
2026-02-23 15:53:39 -0300

    refactor: comment out console log in fotoperiodismo-bajada view script

--- app/Definitions/Fotoperiodismo/blocks/src/fotoperiodismo-bajada/view.js
+++ app/Definitions/Fotoperiodismo/blocks/src/fotoperiodismo-bajada/view.js
@@ -21,5 +21,5 @@
  */
  
 /* eslint-disable no-console */
-console.log("Hello World! (from enfantterrible-fotoperiodismobajada block)");
+// console.log("Hello World! (from enfantterrible-fotoperiodismobajada block)");
 /* eslint-enable no-console */
