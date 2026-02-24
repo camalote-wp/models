78d7be4045513fbbfe476fadcac5a6e0bb00e051
Martín García <tin.geka@gmail.com>
2026-02-24 17:29:20 -0300

    feat: add excerpt meta key to Fotoperiodismo schema

--- app/Definitions/Fotoperiodismo/Meta.php
+++ app/Definitions/Fotoperiodismo/Meta.php
@@ -28,6 +28,11 @@ final class Meta extends AbstractMeta {
                 'single' => true,
                 'show_in_rest' => true,
             ],
+            $prefix . 'excerpt' => [
+                'type' => 'string',
+                'single' => true,
+                'show_in_rest' => true,
+            ],
             $prefix . 'authors' => [
                 'type' => 'array',
                 'single' => true,
