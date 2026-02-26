02fdb3910b0ee33279bf027fe727d6fc29d619c8
Martín García <tin.geka@gmail.com>
2026-02-26 17:54:35 -0300

    feat: implement image rendering and styling for `fotoperiodismo-images` block

--- assets/src/fotoperiodismo/blocks/fotoperiodismo-images/index.js
+++ assets/src/fotoperiodismo/blocks/fotoperiodismo-images/index.js
@@ -3,6 +3,8 @@ import { registerBlockType } from '@wordpress/blocks';
 import { BlockEdit } from './edit';
 import metadata from './block.json';
 
+import './style.scss';
+
 registerBlockType(metadata, {
 	edit: BlockEdit,
 	save: () => null,
