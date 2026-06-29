02480360d04d9da966a3e25a4103aa01ee3923ba
Martín García <tin.geka@gmail.com>
2026-06-29 19:32:03 +0000

    refactor: reorganize Pest setup and update composer scripts for test directory

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,14 +4,19 @@ use Brain\Monkey;
 use Brain\Monkey\Functions;
 use Tests\TestCase;
 
-pest()->extend(TestCase::class)->in('Feature', 'Unit');
-
-beforeEach(function () {
-    Monkey\setUp();
-    Functions\when('__')->returnArg(1);
-    Functions\when('_e')->returnArg(1);
-    Functions\when('_x')->returnArg(1);
-    Functions\when('esc_html')->returnArg(1);
-    Functions\when('esc_attr')->returnArg(1);
-    Functions\when('esc_url')->returnArg(1);
-});
+pest()
+    ->extend(TestCase::class)
+    ->beforeEach(function () {
+        Monkey\tearDown();
+        Monkey\setUp();
+        Functions\when('__')->returnArg(1);
+        Functions\when('_e')->returnArg(1);
+        Functions\when('_x')->returnArg(1);
+        Functions\when('esc_html')->returnArg(1);
+        Functions\when('esc_attr')->returnArg(1);
+        Functions\when('esc_url')->returnArg(1);
+    })
+    ->afterEach(function () {
+        Monkey\tearDown();
+    })
+    ->in('Feature', 'Unit');
